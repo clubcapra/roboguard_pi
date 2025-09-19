5828974f9c4910aeac7162e486a4d0121ab45eb3
ili_the_butterfly <iliana.decarufel@gmail.com>
2025-09-19 14:17:29 -0400

    Added minimal urdf to work with ros_control

---
+++ src/roboguard_bringup/launch/test.launch.py
@@ -0,0 +1,123 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+    IncludeLaunchDescription,
+    RegisterEventHandler,
+    ExecuteProcess,
+    TimerAction,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.event_handlers import OnProcessExit, OnShutdown
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Get the launch directory
+    pkg_roboguard_description = get_package_share_directory("roboguard_description")
+
+    # Get the URDF file
+    urdf_path = os.path.join(pkg_roboguard_description, "urdf", "rove.base.urdf.xacro")
+    robot_desc = Command(["xacro ", urdf_path])
+
+    # Takes the description and joint angles as inputs and publishes
+    # the 3D poses of the robot links
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[
+            {"robot_description": robot_desc},
+        ],
+    )
+    
+    # Controllers
+    controller_nodes = ["diff_drive_controller", "odrive_controller"]
+    # controller_nodes = ["diff_drive_controller"]
+    
+    ###### ROS2 control ######
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("roboguard_bringup"),
+            "config",
+            "roboguard_controllers.yaml",
+        ]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        name="roboguard_controller_manager",
+        output="both",
+        parameters=[
+            {"robot_description": robot_desc},
+            robot_controllers,
+        ]
+    )
+    
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/roboguard_controller_manager",
+        ],
+    )
+
+    def create_controller_node(node_name: str):
+        robot_controller_spawner = Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=[node_name, "--controller-manager", "/roboguard_controller_manager"],
+        )
+
+        # Delay start of robot_controller after `joint_state_broadcaster`
+        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = (
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=joint_state_broadcaster_spawner,
+                    on_exit=[robot_controller_spawner],
+                )
+            )
+        )
+        return delay_robot_controller_spawner_after_joint_state_broadcaster_spawner
+
+    delayed_controller_nodes = list(
+        [create_controller_node(node_name) for node_name in controller_nodes]
+    )
+
+    start_can_cmd = ExecuteProcess(
+        cmd=[[
+            'sudo ip link set down can0; sudo ip link set can0 type can bitrate 500000; sudo ifconfig can0 txqueuelen 1000; sudo ip link set up can0'
+        ]],
+        shell=True
+    )
+
+    stop_can_cmd = ExecuteProcess(
+        cmd=[[
+            'sudo ip link set down can0'
+        ]],
+        shell=True
+    )
+
+    shutdown = RegisterEventHandler(
+        event_handler=OnShutdown(
+            on_shutdown=[stop_can_cmd]
+        )
+    )
+
+    return LaunchDescription(
+        [
+            start_can_cmd,
+            shutdown,
+            robot_state_publisher,
+            control_node,
+            *delayed_controller_nodes,
+        ]
+    )
