94350bdb16bd395d846f0e3aee0ec27a3ab9c018
IliTheButterfly <iliana.decarufel@gmail.com>
2026-03-31 13:50:50 -0400

    Fixed exit on errors and can config setup

--- src/roboguard_bringup/launch/real.launch.py
+++ src/roboguard_bringup/launch/real.launch.py
@@ -10,7 +10,14 @@ from launch.actions import (
 )
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, FindExecutable, AndSubstitution, NotSubstitution
+from launch.substitutions import (
+    Command,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+    FindExecutable,
+    AndSubstitution,
+    NotSubstitution,
+)
 from launch_ros.actions import Node
 from launch.event_handlers import OnProcessExit
 from launch.conditions import UnlessCondition
@@ -18,42 +25,45 @@ from launch.events import Shutdown
 
 handlers = []
 
-def exit_on_crash(node: Node):
+def exit_on_error(actions=[]):
+    def pred(event, context):
+        return [*actions, EmitEvent(event=Shutdown())] if event.returncode != 0 else actions
+    return pred
+
+def exit_on_crash(node: Node, actions=[]):
     """Wraps a node so the launch file shuts down if it exits with a non-zero code."""
     handler = RegisterEventHandler(
         OnProcessExit(
             target_action=node,
-            on_exit=lambda event, context: (
-                [EmitEvent(event=Shutdown())]
-                if event.returncode != 0
-                else []
-            ),
+            on_exit=exit_on_error(actions)
         )
     )
     handlers.append(handler)
     return node
 
+
 def generate_launch_description():
-    IN_DISTROBOX = os.path.exists('/run/host/etc')
-    
+    IN_DISTROBOX = os.path.exists("/run/host/etc")
+
     # Declare parameters
-    use_mock_odrives_dec = DeclareLaunchArgument("use_mock_odrives", default_value="false")
+    use_mock_odrives_dec = DeclareLaunchArgument(
+        "use_mock_odrives", default_value="false"
+    )
     use_mock_odrives = LaunchConfiguration("use_mock_odrives")
-    
+
     use_mock_ovis_dec = DeclareLaunchArgument("use_mock_ovis", default_value="false")
     use_mock_ovis = LaunchConfiguration("use_mock_ovis")
-    
+
     with_ovis_dec = DeclareLaunchArgument("with_ovis", default_value="true")
     with_ovis = LaunchConfiguration("with_ovis")
     real_ovis_present = AndSubstitution(NotSubstitution(use_mock_ovis), with_ovis)
-    
+
     with_rosbag_dec = DeclareLaunchArgument("with_rosbag", default_value="true")
     with_rosbag = LaunchConfiguration("with_rosbag")
-    
+
     # Get the launch directory
     pkg_roboguard_description = get_package_share_directory("roboguard_description")
     pkg_roboguard_bringup = get_package_share_directory("roboguard_bringup")
-    
 
     # Get the URDF file
     urdf_path = os.path.join(pkg_roboguard_description, "urdf", "rove.base.urdf.xacro")
@@ -73,52 +83,49 @@ def generate_launch_description():
             with_ovis,
         ]
     )
-    
+
     # Takes the description and joint angles as inputs and publishes
     # the 3D poses of the robot links
-    robot_state_publisher = exit_on_crash(Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        name="robot_state_publisher",
-        output="both",
-        parameters=[
-            {"robot_description": robot_desc},
-        ],
-    ))
-    
+    robot_state_publisher = exit_on_crash(
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            output="both",
+            parameters=[
+                {"robot_description": robot_desc},
+            ],
+        )
+    )
+
     # Can configuration
-    can_prefix = 'distrobox-host-exec ' if IN_DISTROBOX else ''
-    
-    start_can_cmd = exit_on_crash(ExecuteProcess(
-        cmd=[[
-            f'{can_prefix}sudo ip link set down can0 && '
-            f'{can_prefix}sudo ip link set can0 type can bitrate 500000 && '
-            f'{can_prefix}sudo ifconfig can0 txqueuelen 1000 && '
-            f'{can_prefix}sudo ip link set up can0'
-        ]],
+    can_prefix = "distrobox-host-exec " if IN_DISTROBOX else ""
+
+    start_can_cmd = ExecuteProcess(
+        cmd=[
+            [
+                f"{can_prefix}sudo ip link set down can0 && "
+                f"{can_prefix}sudo ip link set can0 type can bitrate 500000 && "
+                f"{can_prefix}sudo ifconfig can0 txqueuelen 1000 && "
+                f"{can_prefix}sudo ip link set up can0"
+            ]
+        ],
         shell=True,
         condition=UnlessCondition(use_mock_odrives),
-    ))
+    )
 
     stop_can_cmd = ExecuteProcess(
-        cmd=[[f'{can_prefix}sudo ip link set down can0']],
+        cmd=[[f"{can_prefix}sudo ip link set down can0"]],
         shell=True,
     )
 
-    can_shutdown = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=control_node,
-            on_exit=[stop_can_cmd],
-        ),
-        condition=UnlessCondition(use_mock_odrives),
-    )
-    
+
     # Controllers
     controller_nodes = ["odrive_controller", "diff_drive_controller", "ovis_controller"]
     controller_conditions = {
-        "ovis_controller" : IfCondition(real_ovis_present),
+        "ovis_controller": IfCondition(real_ovis_present),
     }
-    
+
     ###### ROS2 control ######
     robot_controllers = PathJoinSubstitution(
         [
@@ -128,15 +135,22 @@ def generate_launch_description():
         ]
     )
 
-    control_node = exit_on_crash(Node(
+    control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
         output="both",
         parameters=[{"robot_description": robot_desc}, robot_controllers],
-    ))
+    )
     
-    
-    joint_state_broadcaster_spawner = exit_on_crash(Node(
+    can_shutdown = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=control_node,
+            on_exit=exit_on_error([stop_can_cmd]),
+        ),
+        condition=UnlessCondition(use_mock_odrives),
+    )
+
+    joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=[
@@ -144,8 +158,8 @@ def generate_launch_description():
             "--controller-manager",
             "/controller_manager",
         ],
-    ))
-    
+    )
+
     delay_joint_state_after_hardware_start = RegisterEventHandler(
         event_handler=OnProcessExit(
             target_action=start_can_cmd,
@@ -159,24 +173,20 @@ def generate_launch_description():
             executable="spawner",
             arguments=[node_name, "-c", "/controller_manager"],
         )
-        
+
         kwargs = {}
         if node_name in controller_conditions.keys():
             kwargs = {"condition": controller_conditions[node_name]}
 
         # Delay start of robot_controller after `joint_state_broadcaster`
-        delay_robot_controller_spawner_after = (
-            RegisterEventHandler(
-                event_handler=OnProcessExit(
-                    target_action=after,
-                    on_exit=[robot_controller_spawner],
-                ),
-                **kwargs
-            )
+        delay_robot_controller_spawner_after = RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=after,
+                on_exit=exit_on_error([robot_controller_spawner]),
+            ),
+            **kwargs,
         )
         return delay_robot_controller_spawner_after, robot_controller_spawner
-
-
 
     delayed_controller_nodes = list()
     last_spawner = joint_state_broadcaster_spawner
@@ -186,28 +196,30 @@ def generate_launch_description():
         last_spawner = spawner
 
     # Twist mux
-    twist_mux = exit_on_crash(Node(
-        package="twist_mux",
-        executable="twist_mux",
-        output="screen",
-        parameters=[pkg_roboguard_bringup + "/config/twist_mux.yaml"],
-        remappings={
-            ("/cmd_vel_out", "/rove/cmd_vel"),
-            ("nav_vel", "/rove/nav/cmd_vel"),
-            ("twist_vel", "/rove/twist/cmd_vel"),
-            ("tank_vel", "/rove/tank/cmd_vel"),
-        },
-    ))
-    
+    twist_mux = exit_on_crash(
+        Node(
+            package="twist_mux",
+            executable="twist_mux",
+            output="screen",
+            parameters=[pkg_roboguard_bringup + "/config/twist_mux.yaml"],
+            remappings={
+                ("/cmd_vel_out", "/rove/cmd_vel"),
+                ("nav_vel", "/rove/nav/cmd_vel"),
+                ("twist_vel", "/rove/twist/cmd_vel"),
+                ("tank_vel", "/rove/tank/cmd_vel"),
+            },
+        )
+    )
+
     # Relay messages from /rove/cmd_vel -> /diff_drive_controller/cmd_vel_unstamped
     cmd_vel_relay = Node(
         package="topic_tools",
         executable="relay",
         name="diff_drive_remap",
         output="screen",
-        arguments=("/rove/cmd_vel", "/diff_drive_controller/cmd_vel_unstamped")
+        arguments=("/rove/cmd_vel", "/diff_drive_controller/cmd_vel_unstamped"),
     )
-    
+
     # Relay messages for odrive enables
     enable_relays = [
         Node(
@@ -215,12 +227,11 @@ def generate_launch_description():
             executable="relay",
             name=f"track_{track}_remap",
             output="screen",
-            arguments=("/rove/enable", f"/odrive_controller/enable/track_{track}_j")
+            arguments=("/rove/enable", f"/odrive_controller/enable/track_{track}_j"),
         )
-        
         for track in ["rl", "rr", "fl", "fr"]
     ]
-    
+
     # Rosbag logging
     rosbag_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -228,7 +239,7 @@ def generate_launch_description():
         ),
         condition=IfCondition(with_rosbag),
     )
-    
+
     return LaunchDescription(
         [
             # Declare args
@@ -236,7 +247,6 @@ def generate_launch_description():
             use_mock_ovis_dec,
             with_ovis_dec,
             with_rosbag_dec,
-            
             # Start nodes
             rosbag_launch,
             start_can_cmd,
