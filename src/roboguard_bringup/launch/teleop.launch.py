37853b40771e397f5f5fef54265f12aaa491ac0a
IliTheButterfly <iliana.decarufel@gmail.com>
2026-04-05 19:27:36 -0400

    Add joy controls (#17)
    Tested so force pushing
    * Added input_manager
    
    * Tested with input_manager with ros-args
    
    * Put all to main
    
    * Added remaps

--- src/roboguard_bringup/launch/teleop.launch.py
+++ src/roboguard_bringup/launch/teleop.launch.py
@@ -1,11 +1,20 @@
+import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     # Get the launch directory
     pkg_roboguard_bringup = get_package_share_directory("roboguard_bringup")
+    pkg_input_manager = get_package_share_directory("input_manager")
+    
+    default_config = os.path.join(pkg_input_manager, 'config', 'default_config.yaml')
+    
+    # Parameters
+    input_config_dec = DeclareLaunchArgument("input_config", default_value=default_config)
+    input_config = LaunchConfiguration("input_config")
     
     joy_config_file = pkg_roboguard_bringup + "/config/teleop_params.yaml"
     
@@ -109,8 +118,26 @@ def generate_launch_description():
         ],
     )
     
+    # input_manager
+    input_manager = Node(
+        package='input_manager',
+        executable='input_manager',
+        name='input_manager',
+        output='screen',
+        parameters=[{
+            'config': input_config,
+            'no_gui': True,
+        }],
+        remappings=[
+            ("/joy_input/steam", "/rove/steamdeck/joy"),
+            ("/joy_input/xbox_bl_controller_laptop", "/rove/xbox/bluetooth/joy"),
+            ("/joy_input/xbox_usb_controller_laptop", "/rove/xbox/usb/joy"),
+        ]
+    )
+    
     return LaunchDescription(
         [
+            input_config_dec,
             joy_mux,
             scheme_selector,
             teleop_mapper,
@@ -119,5 +146,6 @@ def generate_launch_description():
             flippers_teleop,
             enable_node,
             estop_control,
+            input_manager,
         ]
     )
